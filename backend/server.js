68941083e7db54495885053ea86404862bfc98de
NandeeswarPolu <polunandeeswar2002@gmail.com>
2024-11-11 11:25:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -81,6 +81,6 @@ app.get('/register', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT,0.0.0.0, () => {
   console.log(`Server is running on port ${PORT}`);
 });
